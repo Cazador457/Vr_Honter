4aea89212e443587fe592e7af9e09463ae324b3e
Angel <chiv_157@hotmail.com>
2025-09-17 17:00:57 -0600

    Patrol

---
+++ Caza_VR/Assets/Scenary_Test/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public int damage;
+    public Transform[] patrolPoints;
+    private NavMeshAgent _agent;
+    private int _patrolIndex;
+    private float _arrive;
+
+    public virtual void Destination()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        if (patrolPoints.Length > 0)
+        {
+            _patrolIndex = 0;
+            _agent.SetDestination(patrolPoints[_patrolIndex].position);
+        }
+    }
+    void ChangeDestination()
+    {
+        if (!_agent.pathPending && _agent.remainingDistance < _arrive)
+        {
+            _patrolIndex = (_patrolIndex + 1) % patrolPoints.Length;
+            _agent.SetDestination(patrolPoints[_patrolIndex].position);
+        }
+    }
+}
