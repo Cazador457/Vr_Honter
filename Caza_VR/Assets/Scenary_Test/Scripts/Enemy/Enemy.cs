0f0b5b11a1917141f6f66724ac91fe95a2d705c2
Angel <chiv_157@hotmail.com>
2025-10-01 16:41:03 -0600

    Patrol+

--- Caza_VR/Assets/Scenary_Test/Scripts/Enemy/Enemy.cs
+++ Caza_VR/Assets/Scenary_Test/Scripts/Enemy/Enemy.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
-    public float speed;
     public int damage;
+    [Header("Patrol")]
     public Transform[] patrolPoints;
     private NavMeshAgent _agent;
     private int _patrolIndex;
-    private float _arrive;
-
+    private float _arrive = 1f;
+    
+     void Start()
+    {
+        Destination();
+    }
+    void Update()
+    {
+        ChangeDestination();
+    }
     public virtual void Destination()
     {
         _agent = GetComponent<NavMeshAgent>();
@@ -20,6 +28,7 @@ public class Enemy : MonoBehaviour
     }
     void ChangeDestination()
     {
+        if (patrolPoints.Length == 0) return;
         if (!_agent.pathPending && _agent.remainingDistance < _arrive)
         {
             _patrolIndex = (_patrolIndex + 1) % patrolPoints.Length;
--- Caza_VR/Assets/Scenary_Test/Scripts/Enemy/Patrol.cs
+++ Caza_VR/Assets/Scenary_Test/Scripts/Enemy/Patrol.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 using UnityEngine.AI;
 public class Patrol:MonoBehaviour
 {
-    public float speed;
-    public int damage;
+    [Header("Patrol")]
     public Transform[] patrolPoints;
     private NavMeshAgent _agent;
     private int _patrolIndex;
-    private float _arrive;
-
-    public virtual void Destination()
+    private float _arrive = 1f;
+    [Header("Pursuit")]
+    public Transform player;
+    public float rangeVision = 5f;
+    public float visionAngle = 60f;
+    public bool sighted = false;
+    void Destination() //Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         if (patrolPoints.Length > 0)
@@ -20,10 +23,37 @@ public class Patrol:MonoBehaviour
     }
     void ChangeDestination()
     {
+        if (patrolPoints.Length == 0) return;
         if (!_agent.pathPending && _agent.remainingDistance < _arrive)
         {
             _patrolIndex = (_patrolIndex + 1) % patrolPoints.Length;
             _agent.SetDestination(patrolPoints[_patrolIndex].position);
         }
     }
+    /*bool SightedPlayer()
+    {
+        Vector3 rutePlayer = (player.position - transform.position.normalized);
+        float proxPLayer = Vector3.Distance(transform.position, player.position);
+        if (proxPLayer <= rangeVision)
+        {
+            float angleSight = Vector3.Angle(transform.forward, rutePlayer);
+            if(angleSight<=visionAngle)
+        }
+    }
+    void VisionEnemy()  //Update()
+    {
+        if (SightedPlayer())
+        {
+            sighted = true;
+            _agent.SetDestination(player.position);
+        }
+        else
+        {
+            if (sighted)
+            {
+                sighted = false;
+                ChangeDestination();
+            }
+        }
+    }*/
 }
